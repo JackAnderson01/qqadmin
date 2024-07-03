80536b407080315b53d75825b0e80eb2ce90bffa
Hammad ul haq <120801517+Hammadulhaq123@users.noreply.github.com>
2024-07-03 12:48:52 +0500

    Update: Removed navbar admin details

--- src/layout/GlobalLayout.jsx
+++ src/layout/GlobalLayout.jsx
@@ -21,28 +21,13 @@ const GlobalLayout = ({ page }) => {
     <div className="w-screen h-screen flex justify-start items-start">
       <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
       <div className="w-full relative lg:w-[calc(100%-15rem)] xl:w-[calc(100%-18rem)] h-full  overflow-y-auto overflow-x-hidden">
-        <div className="sticky top-0 left-0 w-full h-16 bg-white z-[1000] border-b border-gray-200 flex items-center justify-between lg:justify-end px-4">
+        <div className="sticky top-0 left-0 w-full h-16 bg-white z-[1000] border-b border-gray-200 flex items-center justify-end lg:justify-end px-4">
           <button
             className="block lg:hidden"
             onClick={() => setIsSidebarOpen((prev) => !prev)}
           >
             <CgMenuLeftAlt className="text-3xl text-[#c00000]" />
           </button>
-          <div class="flex gap-3 items-center  py-4 font-normal text-gray-900">
-            <div class="relative bg-[#c00000]/[0.05] rounded-full h-10 w-10">
-              <img
-                class="h-full w-full rounded-full object-cover object-center"
-                src={Logo}
-                alt=""
-              />
-            </div>
-            <div class="text-sm flex flex-col justify-start items-start">
-              <div class="font-semibold text-gray-700 leading-tight">
-                Steven Jobs
-              </div>
-              <div class="text-gray-400">jobs@sailboatui.com</div>
-            </div>
-          </div>
         </div>
         <div className="w-full p-6">{page}</div>
       </div>
