dc8deee4f1027c5c166970d7c2a7eb9cf6ad67c8
mathewgarfield <abdul.hannan@launchbox.pk>
2024-11-28 19:53:59 +0500

    Add Name in States

--- src/components/dashboard/StatesCard.jsx
+++ src/components/dashboard/StatesCard.jsx
@@ -8,25 +8,31 @@ const StatesCard = ({ data, dataLoading }) => {
       id: 1,
       title: "Top Location",
       value: data?.topLocation?.count || "N/A",
+      name: data?.topLocation?._id || "N/A",
       icon: <FaMapMarkerAlt size={18} />,
+      showName: true, // Specify when to show the name
     },
     {
       id: 2,
       title: "Least Location",
       value: data?.leastLocation?.count || "N/A",
+      name: data?.leastLocation?._id || "N/A",
       icon: <IoIosArrowDown size={18} />,
+      showName: true,
     },
     {
       id: 3,
       title: "Most Joined Month",
       value: data?.mostJoinedMonth || "N/A",
       icon: <FaRegCalendarAlt size={18} />,
+      showName: false,
     },
     {
       id: 4,
       title: "Least Joined Month",
       value: data?.leastJoinedMonth || "N/A",
       icon: <IoIosArrowUp size={18} />,
+      showName: false,
     },
   ];
 
@@ -45,11 +51,18 @@ const StatesCard = ({ data, dataLoading }) => {
               {card?.title}
             </h3>
             {dataLoading ? (
-              <div className="text-gray-400  animate-pulse">0...</div>
+              <div className="text-gray-400 animate-pulse">0...</div>
             ) : (
-              <p className="text-[18px] font-bold text-[#c00000]">
-                {card?.value}
-              </p>
+              <div className="flex justify-between w-[170px]">
+                {card.showName && (
+                  <p className="text-[18px] font-bold text-[#c00000]">
+                    {card?.name}
+                  </p>
+                )}
+                <p className="text-[18px] font-bold text-[#c00000]">
+                  {card?.value}
+                </p>
+              </div>
             )}
           </div>
         </div>
